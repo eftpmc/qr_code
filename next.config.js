e56ba2229b47e0d8f1ce977b1073998f8fa1fabd
SaturnEFTPMC <zackhhi@gmail.com>
2022-04-15 10:55:35 -0400

    Adjustments

--- next.config.js
+++ next.config.js
@@ -3,4 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
+module.exports = {
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+};
+
 module.exports = nextConfig
--- pages/index.js
+++ pages/index.js
@@ -13,15 +13,12 @@ export default function Home() {
 
       <main>
         <div id="base">
-          <img id="image" src="/image-qr-code.png"></img>
+          <img id="image" src="/image-qr-code.png" alt="QR Code"></img>
           <p id="first">Improve your front-end skills by building projects</p>
           <p id="second">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
         </div>
       </main>
 
-      <footer >
-
-      </footer>
     </div>
   )
 }
